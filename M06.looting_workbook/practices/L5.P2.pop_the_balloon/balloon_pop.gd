6d6009fe9523313551a9ec32c0cddb8d825c3fb1
paninikeiko <kedelosreyes@s.sfusd.edu>
2024-05-03 12:19:27 -0700

    M6 L5 P1 & P2 Completed

--- M06.looting_workbook/practices/L5.P2.pop_the_balloon/balloon_pop.gd
+++ M06.looting_workbook/practices/L5.P2.pop_the_balloon/balloon_pop.gd
@@ -1,11 +1,13 @@
 extends Area2D
 
+@onready var animation_player: AnimationPlayer = $"AnimationPlayer"
 
 func _input_event(viewport: Node, event: InputEvent, shape_index: int):
 	var event_is_mouse_release: bool = (
 		event is InputEventMouseButton
 		and event.button_index == MOUSE_BUTTON_LEFT
+		and event.is_released() 
 	)
 
 	if event_is_mouse_release:
-		pass
+		queue_free() 
