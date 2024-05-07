15c5a5ecd661c031c440a216c5d708eb264891e9
paninikeiko <kedelosreyes@s.sfusd.edu>
2024-05-07 10:07:26 -0700

    M6 L6 P1 Completed

--- M06.looting_workbook/practices/L6.P1.the_pinata/balloon_pinata.gd
+++ M06.looting_workbook/practices/L6.P1.the_pinata/balloon_pinata.gd
@@ -1,9 +1,7 @@
 extends Area2D
 
-
 func _ready() -> void:
 	randomize()
-
 
 func _input_event(viewport: Node, event: InputEvent, shape_index: int):
 	var event_is_mouse_click: bool = (
@@ -16,17 +14,18 @@ func _input_event(viewport: Node, event: InputEvent, shape_index: int):
 		spawn_candy()
 		input_pickable = false
 
-
 func spawn_candy() -> void:
 	# You have to loop 3 times over the next lines of code!
-	for current_index in range(0):
+	for current_index in range(3):
 		# Complete the variables to calculate a random position in a circle using polar coordinates.
-		var radius := 0.0
-		var angle := 0.0
+		var radius := randf_range(0.0, 100.0)
+		var angle := randf_range(0.0, 2.0 * PI)
 
-		var random_direction := Vector2()
-		var random_position := Vector2()
+		var random_direction := Vector2(1.0, 0.0).rotated(angle)
+		var random_position := Vector2(radius * random_direction)
 
 		# Instantiate and add the candy as a child of the piñata.
 		const CANDY_PACKED_SCENE := preload("candy/candy.tscn")
-		var candy: Node2D = null
+		var candy: Node2D = CANDY_PACKED_SCENE.instantiate()
+		add_child(candy)
+		candy.position = random_position
